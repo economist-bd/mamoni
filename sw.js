cf5573ec08a0ac392958eb3ffc887ddf36a2abbd
MD MONGERUL HAQUE <andrewlaraujo70@gmail.com>
2026-04-03 15:02:20 +0600

    Implement service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'disney-focus-v2'; // আপডেট করলে এখানে v3, v4 করে দিন
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './',
+        './index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// আপডেট হলে নতুন ক্যাশ ব্যবহার
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
